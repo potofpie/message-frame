73ce1bb75505916d3da244b7949dc6feaace1795
potofpie <rec3college@gmail.com>
2020-11-29 21:05:31 -0500

    add more front end shit

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './FileUpload.css';
 
+
+function clickAThing() {
+  this.refs.fileUploader.click();
+}
+
 export default function FileUpload() {
   return (
     <div className='FileUpload-container'>
-        upload your image here
+        {/* <div> upload your image here</div> */}
+        <input className='FileUpload-link' type="file" id="input" multiple></input>
     </div>
   );
 }
