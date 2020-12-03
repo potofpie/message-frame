2b544086b9ac660eb729db4ccace63acde19ebf1
potofpie <you@example.com>
2020-12-02 23:20:46 -0500

    adding more testing code for finding errors

--- api/tests/testing.py
+++ api/tests/testing.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+# import rsa
+# message = b"This is the message to be encrypted"
+# public_key, private_key = rsa.newkeys(1024)
+# encrypted_message = rsa.encrypt(message, private_key)
+# decrypted_message = rsa.decrypt(encrypted_message, public_key)
+# print(encrypted_message, decrypted_message)
 
 from Crypto.PublicKey import RSA
 key = RSA.generate(1024)
@@ -12,26 +17,27 @@ with open('../resources/keys/public', 'w') as filehandle:
 
 with open('../resources/keys/private', 'w') as filehandle:
     filehandle.write(key.exportKey('PEM').decode("utf-8") )
-
+print('keys have been written')
 
 pub = '' 
 pri = '' 
 
+
 with open('../resources/keys/public', 'r') as filehandle:
     pub = filehandle.read()
-
 
 with open('../resources/keys/private', 'r') as filehandle:
     pri = filehandle.read()
 
+print('keys have been read')
 
 # print(str.encode(pub))
 pubKey = RSA.importKey(str.encode(pub))
-encString = pubKey.encrypt(b'this shit',5000)
-
-
-
 priKey = RSA.importKey(str.encode(pri))
 
+# encString = pubKey.encrypt(b'this shit',5000)
+encString = priKey.encrypt(b'this shit',5000)
 
-print(priKey.decrypt(encString).decode("utf-8"))
+
+# print(priKey.decrypt(encString).decode("utf-8"))
+print(pubKey.decrypt(encString).decode("utf-8"))
