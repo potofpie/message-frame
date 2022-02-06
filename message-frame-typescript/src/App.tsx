498a8c7ac768a9acbe47f2cefbec1538c9e48ba6
Bobby Christopher <bobby@ixisdigital.com>
2022-02-06 00:34:15 -0500

    typescript added

---
+++ message-frame-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+export const App:FC = () => {
+  return (
+    <div className="App">
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
